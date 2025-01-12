030e872ee674b128f72995113778b4e779cc9087
Oladayo <oladayobb@gmail.com>
2025-01-12 20:11:12 +0100

    New dependencies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,10 +61,15 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.benchmark.macro)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
+
+    //Coil
+    implementation(libs.coil)
+
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
