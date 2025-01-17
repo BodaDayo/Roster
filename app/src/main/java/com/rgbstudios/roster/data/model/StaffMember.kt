27e00ebbe56ee683767bcafbdc34c2508b2b5586
Oladayo <oladayobb@gmail.com>
2025-01-18 00:01:05 +0100

    updated

--- app/src/main/java/com/rgbstudios/roster/data/model/StaffMember.kt
+++ app/src/main/java/com/rgbstudios/roster/data/model/StaffMember.kt
@@ -2,11 +2,13 @@ package com.rgbstudios.roster.data.model
 
 data class StaffMember(
     val id: Int,
-    val name: String,
+    val firstName: String,
+    val lastName: String,
     val role: Int,
     val unit: Int,
     val avatarUri: String,
     val onCallDates: List<Int>,
+    val gymCallDates: List<Int>,
     val leaveDates: List<Int>,
     val phone: String,
 )
