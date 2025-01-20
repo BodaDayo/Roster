9e7aeb3a177c5d55bd773d1ad8d257e24e28efa6
Oladayo <oladayobb@gmail.com>
2025-01-20 12:08:14 +0100

    set up for raamcosta compose destinations

--- app/src/main/java/com/rgbstudios/roster/MainActivity.kt
+++ app/src/main/java/com/rgbstudios/roster/MainActivity.kt
@@ -3,16 +3,12 @@ package com.rgbstudios.roster
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.rgbstudios.roster.data.model.StaffMember
-import com.rgbstudios.roster.ui.MainScreen
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.rgbstudios.roster.ui.NavGraphs
 import com.rgbstudios.roster.ui.theme.RosterTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +18,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             RosterTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                      MainScreen(modifier = Modifier.padding(innerPadding))
+                    DestinationsNavHost(
+                        navGraph = NavGraphs.root,
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
