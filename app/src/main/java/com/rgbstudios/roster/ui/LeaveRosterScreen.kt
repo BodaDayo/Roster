664c120c74aa838d0ae4571ef495940d02990f72
Oladayo <oladayobb@gmail.com>
2025-01-20 12:07:20 +0100

    New screens added

---
+++ app/src/main/java/com/rgbstudios/roster/ui/LeaveRosterScreen.kt
@@ -0,0 +1,12 @@
+package com.rgbstudios.roster.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+@Destination
+@Composable
+fun LeaveRosterScreen(navigator: DestinationsNavigator) {
+    Text("Leave Roster Screen")
+}
