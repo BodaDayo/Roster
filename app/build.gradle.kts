0c56bc89e3946de60502a11a02f1f6b0e5de71b9
Oladayo <oladayobb@gmail.com>
2025-03-14 19:31:59 +0100

    datastorePreferences, imagecropview, mmkv, supabase-bom, ktorClientAndroid, ktorClientOkhttp, ktorClientPlugins, ktorClientCore, kotlinxSerializationJson, okhttp, workRuntimeKtx, androidx.compose.material, compose-compiler dependencies added
    kotlin version upgraded to 2.1.0
    compileSdk changed to 35
    localProperties supabase data added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,15 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.jetbrains.kotlin.serialization)
+    alias(libs.plugins.androidx.compose.compiler)
 }
 
 android {
     namespace = "com.rgbstudios.roster"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.rgbstudios.roster"
@@ -13,6 +17,27 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+
+        // Load properties from local.properties file
+        val localPropertiesFile = rootProject.file("local.properties")
+        val localProperties = Properties().apply {
+            if (localPropertiesFile.exists()) {
+                load(localPropertiesFile.inputStream())
+            }
+        }
+
+        // Read the properties from local.properties
+        val supabaseUrl = localProperties.getProperty("SUPABASE_URL")
+        val supabaseAnonKey = localProperties.getProperty("SUPABASE_ANON_KEY")
+
+        // Add BuildConfig fields if they exist
+        if (supabaseUrl != null && supabaseAnonKey != null) {
+            buildConfigField("String", "SUPABASE_URL", "\"$supabaseUrl\"")
+            buildConfigField("String", "SUPABASE_ANON_KEY", "\"$supabaseAnonKey\"")
+        } else {
+            // Optionally handle missing properties
+            throw GradleException("Missing SUPABASE_URL or SUPABASE_ANON_KEY in local.properties")
+        }
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -37,10 +62,11 @@ android {
         jvmTarget = "1.8"
     }
     buildFeatures {
+        buildConfig = true
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "2.1.0"
     }
     packaging {
         resources {
@@ -58,6 +84,7 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.benchmark.macro)
     implementation(libs.androidx.navigation.runtime.ktx)
@@ -66,11 +93,43 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
 
+    // Serialization
+    implementation(libs.kotlinx.serialization.json)
+
     //Coil
     implementation(libs.coil)
 
     // Navigation
     implementation(libs.androidx.navigation.compose)
+
+    // Supabase
+    implementation(platform(libs.supabase.bom))
+    implementation(libs.supabase.auth)
+    implementation(libs.supabase.realtime)
+    implementation(libs.supabase.storage)
+    implementation(libs.supabase.postgrest)
+
+    // Ktor
+    implementation(libs.ktor.client.android)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.plugins)
+    implementation(libs.ktor.client.core)
+
+    // OkHttp
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging.interceptor)
+
+    // MMKV
+    implementation(libs.mmkv)
+
+    // Datastore
+    implementation (libs.androidx.datastore.preferences)
+
+    // ImageCrop
+    implementation (libs.imagecropview)
+
+    // WorkManager
+    implementation(libs.androidx.work.runtime.ktx)
 
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
