a048d99f7731c6d624e33194432866b54ff47204
Oladayo <oladayobb@gmail.com>
2025-03-14 19:43:10 +0100

    layout changed, spacer removed

--- app/src/main/java/com/rgbstudios/roster/ui/CustomTopBar.kt
+++ app/src/main/java/com/rgbstudios/roster/ui/CustomTopBar.kt
@@ -1,11 +1,14 @@
 package com.rgbstudios.roster.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -13,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -34,7 +38,7 @@ fun CustomTopBar(navController: NavController) {
         Screen.CallRoster.route -> Screen.CallRoster
         Screen.LeaveRoster.route -> Screen.LeaveRoster
         Screen.StaffList.route -> Screen.StaffList
-        Screen.Settings.route -> Screen.Settings
+        Screen.Resources.route -> Screen.Resources
         else -> null
     }
 
@@ -45,21 +49,30 @@ fun CustomTopBar(navController: NavController) {
             .padding(16.dp),
         contentAlignment = Alignment.CenterStart
     ) {
-        Icon(
-            painter = painterResource(id = R.drawable.roster_icon),
-            contentDescription = stringResource(R.string.app_icon),
-            modifier = Modifier.size(24.dp),
-            tint = Color.Unspecified
-        )
+        Row{
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = currentScreen?.name ?: stringResource(R.string.roster_app),
+                style = MaterialTheme.typography.titleMedium
+            )
+            Spacer(modifier = Modifier.weight(1f))
+        }
 
-        Spacer(modifier = Modifier.width(8.dp))
+        Box(
+            modifier = Modifier
+                .size(24.dp)
+                .clip(CircleShape)
+                .background(Color.White),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.roster_icon),
+                contentDescription = stringResource(R.string.app_icon),
+                modifier = Modifier.size(24.dp),
+                tint = Color.Unspecified
+            )
+        }
 
-        // Title
-        Text(
-            text = currentScreen?.name ?: stringResource(R.string.roster_app),
-            style = MaterialTheme.typography.titleMedium,
-            modifier = Modifier.align(Alignment.Center)
-        )
     }
 
 }
