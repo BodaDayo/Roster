2ca6ef842ee03e6f53e95cb51eef6b4ae87a9b0e
Oladayo <oladayobb@gmail.com>
2025-03-15 21:09:08 +0100

    added LaunchedEffect to show toast when user is not connected to the internet, at launch, from the viewmodel

--- app/src/main/java/com/rgbstudios/roster/MainActivity.kt
+++ app/src/main/java/com/rgbstudios/roster/MainActivity.kt
@@ -6,14 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
+import com.rgbstudios.roster.data.repository.NetworkMonitor
+import com.rgbstudios.roster.data.repository.SupabaseRepository
 import com.rgbstudios.roster.data.viewmodel.RosterViewModel
 import com.rgbstudios.roster.navigation.AppNavigation
-import com.rgbstudios.roster.ui.CustomTopBar
+import com.rgbstudios.roster.ui.components.CustomTopBar
 import com.rgbstudios.roster.ui.components.BottomNavigationBar
 import com.rgbstudios.roster.ui.theme.RosterTheme
+import com.rgbstudios.roster.utils.showLongToast
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +28,21 @@ class MainActivity : ComponentActivity() {
         setContent {
             RosterTheme {
                 val navController = rememberNavController()
-                val rosterViewModel: RosterViewModel = viewModel()
+                val networkMonitor = NetworkMonitor(applicationContext)
+                val supabaseRepository = SupabaseRepository()
+                val rosterViewModel = viewModel<RosterViewModel> {
+                    RosterViewModel(supabaseRepository, networkMonitor)
+                }
+
+                val errorMessage by rosterViewModel.errorMessage.collectAsState()
+                val context = LocalContext.current
+
+                LaunchedEffect(errorMessage) {
+                    errorMessage?.let { message ->
+                        showLongToast(context, message)
+                    }
+                }
+
                 Scaffold(
                     topBar = {
                         CustomTopBar(navController)
