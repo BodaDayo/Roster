349e0d045afa02d2384cfb01fc841a0ea185bf94
Oladayo <oladayobb@gmail.com>
2025-03-23 13:21:17 +0100

    Settings changed to Resources

--- app/src/main/java/com/rgbstudios/roster/navigation/Screen.kt
+++ app/src/main/java/com/rgbstudios/roster/navigation/Screen.kt
@@ -6,5 +6,5 @@ sealed class Screen(val route: String, val iconRes: Int, val name: String) {
     data object CallRoster : Screen("call_roster", R.drawable.ic_user_plus, "Call Roster")
     data object LeaveRoster : Screen("leave_roster", R.drawable.ic_beach, "Leave Roster")
     data object StaffList : Screen("staff_list", R.drawable.ic_people, "Staff List")
-    data object Settings : Screen("settings", R.drawable.ic_settings, "Settings")
+    data object Resources : Screen("resources", R.drawable.ic_resources, "Resources")
 }
